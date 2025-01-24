d3a5ade8b5e1a0f2c2c793f81ef5ae65a150025a
mattot <mattot.builder@gmail.com>
2025-01-24 17:47:57 +0800

    feat: added scripts to toggle overlay for login option

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
-import './scss/styles.scss'
-import * as bootstrap from 'bootstrap'
-import './style.css'
+import "./scss/styles.scss";
+import * as bootstrap from "bootstrap";
+import "./style.css";
 
-console.log('halal-malaysia')
+export function toggleLoginOptionsOverlay(event) {
+  const clickedElement = event.target;
+  const nextLoginOverlay = clickedElement
+    .closest(".login-card")
+    .querySelector(".login-overlay");
+
+  if (!nextLoginOverlay) {
+    return;
+  }
+
+  const closeButton = nextLoginOverlay.querySelector(".btn-close");
+  if (closeButton) {
+    closeButton.addEventListener("click", () => {
+      toggleVisibility(nextLoginOverlay, false);
+    });
+  }
+
+  toggleVisibility(nextLoginOverlay, true);
+}
+
+// Hide and show element in the DOM
+function toggleVisibility(element, show = true) {
+  if (show) {
+    element.classList.remove("d-none");
+  } else {
+    element.classList.add("d-none");
+  }
+}
+
+// Attaching functions to window object
+window.toggleLoginOptionsOverlay = toggleLoginOptionsOverlay;
